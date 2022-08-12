34eb2c5ae0ec3a1fc7406d50545c4e720563f4df
Jakub Hromádko <kubahromadko0506@gmail.com>
2022-08-12 16:18:32 +0200

    add and configure tailwind, postCSS, daisyUI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +10,29 @@ const Home: NextPage = () => {
         <meta name="description" content="Investio app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Investio
+      <h1 className="text-3xl font-bold underline">Investio</h1>
+      <p>test p tag</p>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure>
+          <Image
+            src="https://placeimg.com/400/225/arch"
+            alt="Shoes"
+            width="400"
+            height="225"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
