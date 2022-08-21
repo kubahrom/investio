076b46f1c7f8ed193ce2c6e63c0f51d33b79d4c4
Jakub Hromádko <kubahromadko0506@gmail.com>
2022-08-21 23:48:22 +0200

    editace částky úročení UI

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -5,7 +5,11 @@ import ShowBanks from './Inputs/ShowBanks';
 
 const Filter = () => {
   return (
-    <div className="grid gap-2 rounded-xl border-l-4 border-primary bg-base-100 p-2 shadow-md md:gap-4 md:border-l-8 md:p-4 xl:border-r-8 xl:border-l-0">
+    <div
+      className="grid gap-2 rounded-xl border-l-4 border-primary
+                 bg-base-100 p-2 shadow-md md:gap-4 md:border-l-8 md:p-4 xl:border-r-8
+                 xl:border-l-0"
+    >
       <Search />
       <Order />
       <ShowBanks />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,17 +19,28 @@ const Header = () => {
         <p className="flex items-center py-1 text-lg text-neutral md:text-xl">
           <span className="pr-1">Částka k úročení:</span>
           {edit ? (
-            <input
-              type="text"
-              placeholder="Zadejte částku"
-              className="input input-ghost max-w-xs"
-            />
+            <>
+              <input
+                type="text"
+                autoFocus
+                placeholder="Zadejte částku"
+                id="interest-rate-value"
+                className="input input-primary input-sm ml-2 max-w-[7.5rem]"
+              />
+              <button
+                className="btn btn-primary btn-sm ml-2"
+                onClick={() => setEdit(false)}
+              >
+                Ok
+              </button>
+            </>
           ) : (
             <>
               <span className="font-medium">{valueIntl} Kč</span>
               <button
-                className="edit btn btn-primary btn-sm ml-1 border-primary-content bg-primary-content text-primary after:normal-case after:text-primary-content after:content-['Upravit'] hover:text-primary-content hover:after:text-primary-content"
-                onClick={() => setEdit((e) => !e)}
+                className="edit btn btn-primary btn-sm ml-2 after:normal-case after:text-primary 
+                           after:content-['Upravit'] hover:text-primary-content hover:after:text-primary-content"
+                onClick={() => setEdit(true)}
               >
                 <Edit />
               </button>
